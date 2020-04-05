3b029c1e08c4c57cf4b9ef415889da1f7ae1116b
caoli5288 <caoli5288@gmail.com>
2020-04-05 23:48:36 +0800

    kernel-5.4

--- build.sh
+++ build.sh
@@ -8,9 +8,6 @@ git submodule add https://github.com/openwrt/openwrt.git \
   && cp -a lede/package/lean/ openwrt/package/ \
   && sudo apt update && sudo apt install -y build-essential libncurses-dev \
   && cd openwrt \
-  && git apply ../01-fix-flash.patch \
-  && rm target/linux/ramips/patches-4.14/304-spi-nor-enable-4B-opcodes-for-mx25l25635f.patch \
-  && cp ../999-fix-m25p-shutdown.patch  target/linux/ramips/patches-4.14/ \
   && ./scripts/feeds update -a \
   && ./scripts/feeds install -a \
   && make defconfig \
